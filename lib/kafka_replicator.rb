ec95193feb35b9639c23dcd08752f4b2a340e1a3
Vachman <v.gevorgyan@catawiki.nl>
2019-04-16 14:07:42 +0200

    Require 'railtie'

--- lib/kafka_replicator.rb
+++ lib/kafka_replicator.rb
@@ -1,7 +1,8 @@
+require "kafka"
 require "kafka_replicator/offsets_sync"
+require 'kafka_replicator/railtie' if defined?(Rails)
 require "kafka_replicator/topics_replicator"
 require "kafka_replicator/version"
-require "kafka"
 
 module KafkaReplicator
   class Error < StandardError; end
